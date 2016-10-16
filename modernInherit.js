44024d35f7d721e1321cb01fc657d5a49cda4d31
Mark Naumenko <mark_naumenko@epam.com>
2016-10-16 23:26:38 +0300

    modern added

---
+++ modernInherit.js
@@ -0,0 +1,20 @@
+function extendDeep(parent, child) {
+	var i, toStr = Object.prototype.toString,
+	astr = '[object Array]';
+	child = child || {};
+	for (i in parent) {
+		if (parent.hasOwnProperty(i)) {
+			if (typeof parent[i] === 'object') {
+				child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+				extendDeep(parent[i], child[i]);
+			} else {
+				child[i] = parent[i];
+			}
+		}
+	}
+	return child;
+}
+
+var animal = {class: 'cats'},
+	tiger = extendDeep(animal);
+console.log(tiger.class);
